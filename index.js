13b6f606d51112e5e19fb1624b68ec6e490ec4af
Gerrudo <tom@tomsnetwork.uk>
2020-12-21 18:14:12 +0000

    /signup now provides a token.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const InitiateMongoServer = require("./config/db");
+const user = require("./routes/user");
 
 InitiateMongoServer();
 
@@ -9,10 +10,13 @@ const app = express();
 // PORT
 const PORT = process.env.PORT || 4000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.json({ message: "API Working" });
 });
 
+app.use("/user", user);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server Started on PORT ${PORT}`);
